7010f7fc4f5766948d967ad29636d09048a6363b
Vinicius Benso <vinibenso@gmail.com>
2022-07-04 15:50:07 -0300

    final config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import './styles/global.scss';
+
 export function App() {
+  
   return <h1>Opa ali</h1>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@
 import { render } from 'react-dom';
 import { App } from './App';
 
-render(<h1>Testretg</h1>, document.getElementById('root'))  
+render(<App/>, document.getElementById('root'))  
